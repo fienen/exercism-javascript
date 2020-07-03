84b5c0b9d1f6bf454b4a95371652d52d1f7d10f1
Michael Fienen <fienen@gmail.com>
2020-07-02 20:04:38 -0500

    Completes hamming exercise

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+export const compute = ( s1, s2 ) => {
+  let strand1 = s1.split('');
+  let strand2 = s2.split('');
+  let dist = 0;
+
+  if ( s1 === '' && s2.length > 0 ) {
+    throw Error('left strand must not be empty');
+  } else if ( s2 === '' && s1.length > 0 ) {
+    throw Error('right strand must not be empty');
+  } else if ( s1.length !== s2.length ) {
+    throw Error('left and right strands must be of equal length');
+  } else {
+    strand1.forEach(function(rna, i) {
+      dist = rna != strand2[i] ? dist + 1 : dist;
+    });
+
+    return dist;
+  }
 };
