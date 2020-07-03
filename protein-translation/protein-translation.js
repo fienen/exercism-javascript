26187e7993a6af2cfd2b29599e932f156a2c6c53
Michael Fienen <fienen@gmail.com>
2020-07-02 20:31:55 -0500

    Check single codon

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -3,6 +3,45 @@
 // convenience to get you started writing code faster.
 //
 
-export const translate = () => {
-  throw new Error("Remove this statement and implement this function");
+export const translate = ( codon ) => {
+  let proteins = [];
+  let stop = false;
+
+  switch( codon ) {
+    case 'UCU':
+    case 'UCC':
+    case 'UCA':
+    case 'UCG':
+      proteins.push('Serine');
+      break;
+    case 'UUU':
+    case 'UUC':
+      proteins.push('Phenylalanine');
+      break;
+    case 'UUA':
+    case 'UUG':
+      proteins.push('Leucine');
+      break;
+    case 'UAU':
+    case 'UAC':
+      proteins.push('Tyrosine');
+      break;
+    case 'UGU':
+    case 'UGC':
+      proteins.push('Cysteine');
+      break;
+    case 'AUG':
+      proteins.push('Methionine');
+      break;
+    case 'UGG':
+      proteins.push('Tryptophan');
+      break;
+    case 'UAA':
+    case 'UAG':
+    case 'UGA':
+      stop = true;
+      break;
+  }
+
+  return proteins;
 };
