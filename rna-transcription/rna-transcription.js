9c4292660d6cf52fb1ef205eb56fec7eccfc61c7
Michael Fienen <fienen@gmail.com>
2020-07-03 20:18:59 -0500

    Change from split() to a spread operator

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,5 +1,5 @@
 const RNA = { C: 'G',T: 'A',G: 'C',A: 'U' };
 
-export const toRna = rna => {
-  return rna.split('').map(r => RNA[r]).join('');
+export const toRna = rna  => {
+  return [...rna].map(r => RNA[r]).join('');
 };
