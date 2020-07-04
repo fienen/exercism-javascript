a07f2bee7bf33ffa55723ea383d5445e06f065e1
Michael Fienen <fienen@gmail.com>
2020-07-03 20:34:04 -0500

    Finish protein translation exercise

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -3,49 +3,54 @@
 // convenience to get you started writing code faster.
 //
 
-export const translate = ( codon ) => {
+export const translate = ( rna ) => {
+  let codons = rna === undefined ? [] : rna.match(/.{1,3}/g);
   let proteins = [];
   let stop = false;
 
-  switch( codon ) {
-    case 'UCU':
-    case 'UCC':
-    case 'UCA':
-    case 'UCG':
-      proteins.push('Serine');
-      break;
-    case 'UUU':
-    case 'UUC':
-      proteins.push('Phenylalanine');
-      break;
-    case 'UUA':
-    case 'UUG':
-      proteins.push('Leucine');
-      break;
-    case 'UAU':
-    case 'UAC':
-      proteins.push('Tyrosine');
-      break;
-    case 'UGU':
-    case 'UGC':
-      proteins.push('Cysteine');
-      break;
-    case 'AUG':
-      proteins.push('Methionine');
-      break;
-    case 'UGG':
-      proteins.push('Tryptophan');
-      break; 
-    case undefined:
-      break;
-    case 'UAA':
-    case 'UAG':
-    case 'UGA':
-      stop = true;
-      break;
-    default:
-      throw Error('Invalid codon');
-  }
+  codons.forEach(codon => {
+    if(!stop) {
+      switch( codon ) {
+        case 'UCU':
+        case 'UCC':
+        case 'UCA':
+        case 'UCG':
+          proteins.push('Serine');
+          break;
+        case 'UUU':
+        case 'UUC':
+          proteins.push('Phenylalanine');
+          break;
+        case 'UUA':
+        case 'UUG':
+          proteins.push('Leucine');
+          break;
+        case 'UAU':
+        case 'UAC':
+          proteins.push('Tyrosine');
+          break;
+        case 'UGU':
+        case 'UGC':
+          proteins.push('Cysteine');
+          break;
+        case 'AUG':
+          proteins.push('Methionine');
+          break;
+        case 'UGG':
+          proteins.push('Tryptophan');
+          break; 
+        case undefined:
+          break;
+        case 'UAA':
+        case 'UAG':
+        case 'UGA':
+          stop = true;
+          break;
+        default:
+          throw Error('Invalid codon');
+      }
+    }
+  });
 
   return proteins;
 };
