e5e3481232a151f1924209ed91f87e9c0937634c
Michael Fienen <fienen@gmail.com>
2020-07-06 20:14:48 -0500

    Finish scrabble-score, start pangram

--- scrabble-score/scrabble-score.js
+++ scrabble-score/scrabble-score.js
@@ -3,6 +3,14 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = () => {
-  throw new Error("Remove this statement and implement this function");
+const charMap = {a: 1, b: 3, c: 3,d: 2,e: 1,f: 4,g: 2,h: 4,i: 1,j: 8,k: 5,l: 1,m: 3,n: 1,o: 1,p: 3,q: 10,r: 1,s: 1, t: 1,u: 1,v: 4,w: 4,x: 8,y: 4,z: 10};
+
+export const score = word => {
+  let score = 0;
+
+  [...word].map(function(char) {
+    score += charMap[char.toLowerCase()];
+  });
+
+  return score;
 };
