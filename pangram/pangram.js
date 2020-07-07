e0f13942cd56a25991e36b746b3227f293f86b0e
Michael Fienen <fienen@gmail.com>
2020-07-06 20:33:27 -0500

    Finish pangram

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isPangram = sentence => {
+  return alphabet.every(char => sentence.toLowerCase().includes(char));
 };
