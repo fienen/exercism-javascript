dc702f3049c764d4a6dda3943b4dcd5900909467
Michael Fienen <fienen@gmail.com>
2020-07-07 23:29:36 -0500

    Finish matrix

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,18 +1,31 @@
-//
-// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Matrix {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(str) {
+    // Split the string by newlines, then each of those by spaces, parsing the string into an integer
+    this.matrix = str.split('\n').map(row => row.split(' ').map(i => parseInt(i)));
   }
 
   get rows() {
-    throw new Error("Remove this statement and implement this function");
+    // Returning rows is just returning the normal, parsed out matrix
+    return this.matrix;
   }
 
   get columns() {
-    throw new Error("Remove this statement and implement this function");
+    // Find out how tall and wide the matrix is to rotate it 90 degrees
+    const height = this.matrix.length;
+    const width = this.matrix[0].length;
+    let columned = [];
+    
+    // This feels inefficient, but does work. Maybe a smarter way to do it with map() or splice()...
+    for(let i = 0; i < width; i++) {
+      let row = [];
+      
+      for(let ii = 0; ii < height; ii++) {
+        row.push(this.matrix[ii][i]);
+      }
+      
+      columned.push(row);
+    }
+    
+    return columned;
   }
 }
